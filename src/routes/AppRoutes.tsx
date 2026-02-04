ebaabea6ea4c9ea1d30e6696dcb878136adbe480
David Ericson <davidestudofcg@gmail.com>
2026-02-04 08:56:36 -0300

    feat: add protected Home route
    Import Home and PrivateRoute into AppRoutes and add a /home route wrapped with PrivateRoute so the Home page is only accessible to authenticated users.

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "../pages/auth/Login.js";
 import SignUp from "../pages/auth/SignUp.js";
+import Home from "../pages/home/home.js";
+import { PrivateRoute } from "./PrivateRoute.js";
 
 export default function AppRoutes () {
     return (
         <Routes>
             <Route path="/login" element={<Login/>}/>
             <Route path="/signup" element={<SignUp/>}/>
+            <Route path="/home" element={
+                <PrivateRoute>
+                    <Home/>
+                </PrivateRoute>
+            }/>
         </Routes>
     )
 }
