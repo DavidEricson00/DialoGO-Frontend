e7682c8e9d8a987056fc44e0d4c6d4fd9f92eb5a
David Ericson <davidestudofcg@gmail.com>
2026-02-09 14:44:24 -0300

    feat: fetch current user and pass to Header

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react"
 import Header from "../../components/layout/Header"
+import { User } from "../../types/User"
+import { getMe } from "../../services/user.service"
 
 export default function Home() {
+    const [user, setUser] = useState<User | null>(null)
+
+    useEffect(() => {
+        getMe()
+            .then(data => setUser(data))
+            .catch(err => console.error(err))
+    })
+
+    function openUserMenu(){
+        console.log("Abrir menu")
+    }
+
     return(
         <>
-            <Header/>
+            {user && (
+                <Header
+                    user={user}
+                    onClick={openUserMenu}
+                />
+            )}
             <div className="mt-38">
                 <p>Home</p>
             </div>
