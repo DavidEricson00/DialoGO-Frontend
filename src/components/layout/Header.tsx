61b1af4956dec1d6ffcd8b40d7d809e7b94f88b0
David Ericson <davidestudofcg@gmail.com>
2026-02-09 14:43:25 -0300

    feat: added user prop to header, dynamic avatar and username

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,12 @@
-export default function Header() {
+import { User } from "../../types/User"
+import { getAvatarPath } from "../../utils/getAvatarPath"
+
+type HeaderProps = {
+    user: User,
+    onClick?: () => void
+}
+
+export default function Header({ user, onClick }: HeaderProps) {
     return (
         <header className="fixed left-0 top-0 w-full h-24 bg-blue-600 text-white flex flex-row justify-between items-center px-10 shadow-lg z-50">
             <div className="bg-blue-700/50 py-2 px-4 rounded-lg border border-blue-200">
@@ -6,16 +14,16 @@ export default function Header() {
             </div>
 
             <div className="flex flex-col items-center justify-center gap-1 group cursor-pointer">
-                <div className="relative">
+                <div className="relative" onClick={onClick}>
                     <img 
-                        src="src/assets/avatars/default.png" 
+                        src={getAvatarPath(user.avatar)}
                         alt="Perfil" 
                         className="w-12 h-12 rounded-full object-cover border-3 border-white/50 group-hover:border-white transition-all"
                     />
                     <span className="absolute bottom-0 right-0 w-3 h-3 bg-green-400 border-2 border-blue-400 rounded-full"></span>
                 </div>
                 <p className="text-xs font-medium text-blue-100 group-hover:text-white transition-colors">
-                    Usuário
+                    {user.username}
                 </p>
             </div>
         </header>
