1f9a1f7a34d54547f04f9ae88dc4cf3be8ed7618
David Ericson <davidestudofcg@gmail.com>
2026-02-09 14:42:32 -0300

    feat: move avatars to public and update paths

--- src/constants/avatarPath.ts
+++ src/constants/avatarPath.ts
@@ -1,13 +1,13 @@
 import { AvatarType } from "../types/AvatarType";
 
 export const AVATAR_MAP: Record<AvatarType, string> = {
-    avatar_1: "../assets/avatars/default.png",
-    avatar_2: "../assets/avatars/alien.png",
-    avatar_3: "../assets/avatars/smile.png",
-    avatar_4: "../assets/avatars/duck.png",
-    avatar_5: "../assets/avatars/elephant.png",
-    avatar_6: "../assets/avatars/dinossaur.png",
-    avatar_7: "../assets/avatars/goblin.png",
-    avatar_8: "../assets/avatars/oni.png",
-    avatar_9: "../assets/avatars/robot.png",
+    avatar_1: "/avatars/default.png",
+    avatar_2: "/avatars/alien.png",
+    avatar_3: "/avatars/smile.png",
+    avatar_4: "/avatars/duck.png",
+    avatar_5: "/avatars/elephant.png",
+    avatar_6: "/avatars/dinossaur.png",
+    avatar_7: "/avatars/goblin.png",
+    avatar_8: "/avatars/oni.png",
+    avatar_9: "/avatars/robot.png",
 }
